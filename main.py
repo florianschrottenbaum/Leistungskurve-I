dc42a2194b8038fdc5c3034cb8e7d7105dd072e8
florianschrottenbaum <sf3826@mci4me.at>
2024-04-11 14:51:08 +0200

    changed code to english

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import load_data
 import numpy as np
 import matplotlib as plt
-daten = load_data.load_data('activity.csv')
-Heartrate = (daten['HeartRate'])
-xachse = np.arange(1, 1805)
-plt.pyplot.plot(xachse,Heartrate)
+data = load_data.load_data('activity.csv')
+HeartRate = (data['HeartRate'])
+xaxis = np.arange(1, 1805)
+plt.pyplot.plot(xaxis,HeartRate)
 plt.pyplot.show()
