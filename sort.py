3c6ab005a1678daed15fd891cacaa0496b681c2a
florianschrottenbaum <sf3826@mci4me.at>
2024-04-11 15:36:21 +0200

    edited sort.py

--- sort.py
+++ sort.py
@@ -1,15 +1,6 @@
-def bubble_sort(list):
+def sort(list):
     for i in range(len(list)-1):
         for j in range(len(list)-1):
-            if list[j] > list[j+1]:
+            if list[j] < list[j+1]:
                 list[j], list[j+1] = list[j+1], list[j]
-    return list
-
-def selection_sort(list):
-    for i in range(len(list)-1, -1, -1):
-        i_max = i
-        for j in range(0, i):
-            if list[j] > list[i_max]:
-                i_max = j
-        list[i], list[i_max] = list[i_max], list[i]
     return list
