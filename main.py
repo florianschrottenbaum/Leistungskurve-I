4267ec0f33ad80d40ecab5b20b5d41163580612f
florianschrottenbaum <sf3826@mci4me.at>
2024-04-11 16:36:58 +0200

    added plot title

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ labelsxaxis = ['0','5','10','15','20','25','30']
 plt.pyplot.xticks(xaxisticks, labelsxaxis)
 plt.pyplot.xlabel('Time [min]')
 plt.pyplot.ylabel('Power [W]')
+plt.pyplot.title('Power-Curve')
 #saving the plot
 plt.pyplot.savefig('figures/PowerOriginal.png')
 #showing Plot
